05cdf655cbd7c27121c87da82003a1f25e4b8fff
João Pedro Macena <joaomacenactba@gmail.com>
2025-03-20 20:57:58 -0300

    Delete consumoMédio.py

--- consumoMédio.py
+++
@@ -1,5 +0,0 @@
-distanciaPercorrida  = float(input("Digite a distância percorrida: "))
-combustivelGasto = float(input("Quanto combustível foi gasto? "))
-
-consumoMedio = distanciaPercorrida / combustivelGasto
-print(f"O consumo médio do veículo foi de: {consumoMedio:.2f}km/l")

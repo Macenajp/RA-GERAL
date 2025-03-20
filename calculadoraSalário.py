2bb18b1c1dc7cba52c0376585f049cb10ab9fafa
João Pedro Macena <joaomacenactba@gmail.com>
2025-03-20 20:57:46 -0300

    Delete calculadoraSalário.py

--- calculadoraSalário.py
+++
@@ -1,7 +0,0 @@
-salarioMinimo = 1509.00
-salarioDoProfissional = float(input("Digite o salário: "))
-
-salarioAnual = salarioDoProfissional * 12
-quantidadeDeSalarios = salarioAnual / 1509
-
-print(f"O profissional recebe {quantidadeDeSalarios:.2f} salários mínimos")

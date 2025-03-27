8ed5a6a6c00297cac0f09df6f3100b495f12a570
João Pedro Macena <joaomacenactba@gmail.com>
2025-03-26 23:23:20 -0300

    Create medidaAbóbora.py

---
+++ TDE2/medidaAbóbora.py
@@ -0,0 +1,7 @@
+# Questão 
+diametro = float(input("Digite o diâmetro da abóbora em cm: "))
+
+if diametro >= 15 and diametro < 20:
+    print("Abóbora do tipo médio.")
+else:
+    print("Produto fora das medidas.")

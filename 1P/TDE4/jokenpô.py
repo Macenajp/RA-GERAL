7ef83681b25579d98b49e1b618f6181ded2b906e
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-02 08:13:00 -0300

    Update jokenpô.py

--- 1P/TDE4/jokenpô.py
+++ 1P/TDE4/jokenpô.py
@@ -8,4 +8,5 @@ Humano        Máquina
 Máquina       Máquina
 
 Durante cada "execução", só poderá ter um estilo de jogo, exemplo: Humano vs Máquina.
+Depois de um determinado número de partidas, terá que aparecer uma pergunta para o usuário, se ele quer "CONTINUAR" ou "SAIR".
 '''

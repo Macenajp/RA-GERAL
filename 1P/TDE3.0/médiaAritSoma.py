e80b8437ca19165e86d097f68053aa22c6575241
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-01 23:22:06 -0300

    Update médiaAritSoma.py

--- 1P/TDE3.0/médiaAritSoma.py
+++ 1P/TDE3.0/médiaAritSoma.py
@@ -4,7 +4,7 @@ contador = 1
 soma = 0
 
 while contador <= 10:
-    entradas = int(input('Insira um número: '))
+    entradas = int(input("Insira um número: "))
     contador = contador + 1
     soma += entradas
 

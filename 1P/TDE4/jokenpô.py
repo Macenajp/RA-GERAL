2d17e4510ccf80eb80f8115bdd3f2a03340ae5ac
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-02 08:11:39 -0300

    Update jokenpô.py

--- 1P/TDE4/jokenpô.py
+++ 1P/TDE4/jokenpô.py
@@ -1,1 +1,11 @@
-# Trabalho colaborativo
+# Trabalho colaborativo da criação do "Jokenpô":
+
+'''
+Opções de jogo:
+Player 1      Player 2
+Humano        Humano
+Humano        Máquina
+Máquina       Máquina
+
+Durante cada "execução", só poderá ter um estilo de jogo, exemplo: Humano vs Máquina.
+'''

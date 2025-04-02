ba7ec2a464b7547e8c1acd9d98c24b4ab4573162
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-01 23:05:48 -0300

    Update moedasCompra.py

--- 1P/TDE3.0/moedasCompra.py
+++ 1P/TDE3.0/moedasCompra.py
@@ -1,16 +1,34 @@
 # Questão 10 da lista:
-moeda = int(input("Qual moeda você quer comprar. Dolár (1), Euro (2) ou Libra (3). "))
-valor = float(input("Quanto você quer adiquirir? "))
 
-# Moedas:
-dolar = valor * 5.74
-euro = valor * 6.19
-libra = valor * 7.42
+# Opções de moedas:
+dolar = 5.76
+euro = 6.19
+libra = 7.42
 
-# Cálculo da compra:
-while moeda >= 1:
-    dolar = valor * 5.74
-    if dolar <= 1000 * 0.95:
-        print("Você receberá:", dolar, ".A empresa ficará com 5% das taxas")
-    elif dolar >= 1000 * 0.97:
-        print("Você receberá: ", dolar, ".A empresa ficará com 3% das taxas")
+# Escolha da moeda pelo usuário:
+moeda = 0
+while moeda < 1 or moeda > 3:
+    print('Escolha uma moeda para comprar:')
+    print('1 - Dólar')
+    print('2 - Euro')
+    print('3 - Libra')
+
+    moeda = int(input('Qual moeda você quer comprar? '))
+    total = float(input('Digite a quantidade desejada: '))
+
+# Resultado final da "compra":
+if moeda  == 1:
+    if total < 1000:
+        print(f"O valor final é: {total * dolar * 1.05:.2f}")
+    else:
+        print(f"O valor final é: {total * dolar * 1.03:.2f}")
+elif  moeda  ==  2:
+    if total < 1000:
+        print(f"O valor final é: {total * euro * 1.05:.2f}")
+    else:
+        print(f"O valor final é: {total * euro * 1.03:.2f}")
+elif moeda  == 3:
+    if total < 1000:
+        print(f"O valor final é: {total * libra * 1.05:.2f}")
+    else:
+        print(f"O valor final é: {total * libra * 1.03:.2f}")

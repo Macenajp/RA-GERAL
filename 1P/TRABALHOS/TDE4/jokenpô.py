dfa699ce6f70536f46c8691da9b6c65d135256f6
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-02 10:09:04 -0300

    Update jokenpô.py

--- 1P/TRABALHOS/TDE4/jokenpô.py
+++ 1P/TRABALHOS/TDE4/jokenpô.py
@@ -1,1 +1,20 @@
-# Início do projeto:
+# Biblioteca:
+import random
+
+# Entrada e dados:
+inicioDoJogo = input("Gostaria de iniciar o jogo (s/n)?:")
+
+if inicioDoJogo == "s":
+    print("1 - Humano x Humano")
+    print("2 - Humano x Bot")
+    print("3 - Bot x Bot")
+else:
+    print("Volte logo!")
+
+jogadaContinuar = int(input("Qual será o modo de jogo? "))
+print = int(input(inicioDoJogo))
+if inicioDoJogo == "s":
+    print("1 - Humano x Humano")
+    print("2 - Humano x Bot")
+    print("3 - Bot x Bot")
+

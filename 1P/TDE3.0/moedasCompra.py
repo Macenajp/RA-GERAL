d3e4f5b2be91cfbb1322dff111353ab72828d6fb
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-01 23:23:54 -0300

    Update moedasCompra.py

--- 1P/TDE3.0/moedasCompra.py
+++ 1P/TDE3.0/moedasCompra.py
@@ -11,7 +11,7 @@ while moeda < 1 or moeda > 3:
     print("Escolha uma dessas moedas para comprar:')
     print("1 - Dólar')
     print("2 - Euro')
-    print('3 - Libra')
+    print("3 - Libra')
 
     moeda = int(input('Qual moeda você quer deseja comprar? '))
     total = float(input('Digite a quantidade desejada: '))

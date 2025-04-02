7d1c9d4413d91f44f2eed69441e9c7596645d975
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-01 23:09:34 -0300

    Create multTrês.py

---
+++ 1P/TDE3.0/multTrês.py
@@ -0,0 +1,12 @@
+# Questão 9 da lista:
+
+li = float(input('Insira um limite incical: '))
+lf = float(input('Insira um limite final: '))
+
+inicioLi = li + 1 if (li + 1) % 3 != 0 else li + 1
+contador = inicioLi
+
+while contador < lf:
+    if contador % 3 == 0:
+        print('Número:', contador)
+    contador += 1

b03a666a4f019147c1c0d65e366cb542503d99c7
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-01 23:21:43 -0300

    Update numImparPersonalizado.py

--- 1P/TDE3.0/numImparPersonalizado.py
+++ 1P/TDE3.0/numImparPersonalizado.py
@@ -3,5 +3,5 @@ numero = int(input('Insira um número inteiro:'))
 contador = 1
 while contador <= numero:
     if contador % 2 != 0:
-        print(f"Número:, {contador:.2f})
+        print(f"Número:, {contador:.2f}")
     contador = contador + 1

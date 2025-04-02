cc158b48e90a3fec17b12d6ce7192b89eb11017f
João Pedro Macena <joaomacenactba@gmail.com>
2025-04-02 08:26:14 -0300

    Create jokenpôPlano.py

---
+++ 1P/TRABALHOS/TDE4/jokenpôPlano.py
@@ -0,0 +1,19 @@
+# Trabalho colaborativo da criação do "Jokenpô":
+
+'''
+Opções de jogo:
+Player 1      Player 2
+Humano        Humano
+Humano        Máquina
+Máquina       Máquina
+
+Durante cada "execução", só poderá ter um estilo de jogo, exemplo: Humano vs Máquina.
+Depois de um determinado número de partidas, terá que aparecer uma pergunta para o usuário, se ele quer "CONTINUAR" ou "SAIR".
+'''
+
+'''
+Modo de jogo:
+- Para cada "round", solicitará uma jogada (pedra, tesoura ou papel).
+- Depois de um determinado número de jogadas, deverá coletar os dados e apresentar o placar no final da partida e indicar o vencedor.
+-  Caso o player queira "continuar", haverá um número determinado de jogadas posteriormente. Se escolher "sair", irá mostrar direto, o placar.
+'''

1fd9a80964bf88bf174f119af5031e489c3caf21
João Pedro Macena <joaomacenactba@gmail.com>
2025-05-14 08:38:19 -0300

    Create Matriz.py

---
+++ 1P/Revisão/Matriz/Matriz.py
@@ -0,0 +1,10 @@
+m = [[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15]]
+
+for linha in range(len(m)):
+    for coluna in range(len(m[0])):
+        print(m[linha][coluna])        
+        #print(m[linha][coluna], end='')  

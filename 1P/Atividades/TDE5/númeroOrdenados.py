20e228abb6420a918f04a063063c35016f3d2733
João Pedro Macena <joaomacenactba@gmail.com>
2025-05-15 00:51:09 -0300

    Create númeroOrdenados.py

---
+++ 1P/Atividades/TDE5/númeroOrdenados.py
@@ -0,0 +1,16 @@
+# Questão 13 da lista:
+vetor = []
+
+for i in range(20):
+    a = int(input("Insira um número: "))
+    vetor.append(a)
+posicoes = len(vetor)
+
+for i in range(posicoes - 1):
+    menor = i
+    for a in range(i + 1, posicoes):
+        if vetor[a] < vetor[menor]:
+            menor = a
+    vetor[i], vetor[menor] = vetor[menor], vetor[i]
+
+print("Vetor ordenado:", vetor)

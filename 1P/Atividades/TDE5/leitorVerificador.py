be842cf2a1359d791f416f0043e9bad30c5a2e87
João Pedro Macena <joaomacenactba@gmail.com>
2025-05-15 00:26:39 -0300

    Delete 1P/Atividades/TDE5/leitorVerificador.py

--- 1P/Atividades/TDE5/leitorVerificador.py
+++
@@ -1,9 +0,0 @@
-# Questão 3 da lista:
-num1 = float(input("Digite o primeiro número como 'base': "))
-num2 = float(input("Digite o segundo número que será usado para a soma com o próximo: "))
-num3 = float(input("Digite o terceiro número que fará parte da soma: "))
-
-if num1 > (num2 + num3):
-    print(f"{num1} é maior que a soma de {num2} e {num3} ({num2 + num3}).")
-else:
-    print(f"{num1} não é maior que a soma de {num2} e {num3} ({num2 + num3}).")

b390d56718f7712ac9a037093b679b9ab9682e0b
João Pedro Macena <joaomacenactba@gmail.com>
2025-05-28 08:37:52 -0300

    Create descubraNumero.py

---
+++ 1P/REVISÃO/descubraNumero.py
@@ -0,0 +1,26 @@
+import random
+def numero():
+    return random.randint(1, 50)
+
+def jogadas():
+    resposta = numero()
+    tentativa = 5
+    chutes = 5
+
+    print("Descubra o número! - Entre 0 e 50, você tem cinco tentativas.")
+    while chutes > resposta or chutes < resposta:
+        tentativa += 1
+        chute = int(input("Qual o seu chute? "))
+        if chute > resposta or chute < resposta:
+            print("Vish, você errou.")
+        else:
+            print("Você acertou. Parabéns!"
+                  "<=====================>")
+            break
+
+        if resposta > chute:
+            print("Tente um número maior que esse.")
+        else:
+            print("Tente um número menor que esse.")
+while True:
+    jogadas()
